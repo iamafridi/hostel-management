994ce6705830c85339e56d351cbd9573ce6d8836
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-03 19:51:45 +0600

    Refactored user validation , student route ,controller and service

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -1,14 +1,11 @@
 import z from 'zod';
 
 const userValidationSchema = z.object({
-    id: z.string(),
-    password = z.string().max(20, {
+    password = z.string({
+        invalid_type_error: 'Password Needs To Be String'
+    }).max(20, {
         message: 'Password can not be more than 20 characters',
-    }),
-    needsPasswordChange: z.boolean().optional().default(true),
-    role: z.enum(['student', 'faculty', 'admin']),
-    status: z.enum(['in-progress', 'blocked']).default('in-progress'),
-    isDeleted: z.boolean().optional().default(false),
+    }).optional(),
 });
 
 export const userValidation = {
