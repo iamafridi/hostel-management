0e4907a02380aec79cd7f269c1916df74634a03b
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-06 01:11:21 +0600

    Created Academic Semester Model

---
+++ src/app/modules/academicSemester/academicSemester.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from 'mongoose';
+import { TAcademicSemester, TMonth } from './academicSemester.interface';
+
+export const months: TMonths[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+];
+
+const academicSemesterSchema = new Schema<TAcademicSemester>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        year: {
+            type: Date,
+            required: true,
+        },
+        code: {
+            type: String,
+            required: true,
+        },
+        startMonth: {
+            type: String,
+            enum: months,
+        },
+        endMonth: {
+            type: String,
+            enum: months,
+        },
+    },
+    {
+        timestamps: true, // for the createdAt and Updated At
+    },
+);
+
+
+export const AcademicSemester = model<TAcademicSemester>('AcademicSemester', academicSemesterSchema)
