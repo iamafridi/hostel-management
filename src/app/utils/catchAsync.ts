c5a5deb2cdf9becea83d58a7bc4d7ad448d590e2
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-05 23:31:56 +0600

    fixed bug

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, RequestHandler, Response } from 'express';
 
 //Higher order function
-const catchAsync = (fn: RequestHandler) => {
+const catchAsync = (func: RequestHandler) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+        Promise.resolve(func(req, res, next)).catch((err) => next(err));
     };
 };
 
