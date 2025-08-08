4fb648f4159ec6f3067bba05781bf234c182b78b
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-08 20:17:46 +0600

    Fixed Bugs

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -21,7 +21,22 @@ const findLastStudentId = async () => {
 export const generateStudentId = async (payload: TAcademicSemester) => {
     console.log(await findLastStudentId);
     // first time 0000
-    const currentId = await findLastStudentId || (0).toString();
+    let currentId = (0).toString();  //0000 by default  , used let coz eta reassigned hobe
+
+    const lastStudentId = await findLastStudentId()
+    const lastStudentSemesterCode = lastStudentId?.substring(4, 6) //01
+    const lastStudentYear = lastStudentId?.substring(0, 4) //2030
+    //comparision
+    const currentSemesterCode = payload.code;
+    const currentYear = payload.year;
+
+    if (lastStudentId && lastStudentSemesterCode === currentSemesterCode && lastStudentYear === currentYear) {
+
+        currentId = lastStudentId.substring(6);
+
+    }
+
+
     let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
     incrementId = `${payload.year}${payload.code}${incrementId}`;
