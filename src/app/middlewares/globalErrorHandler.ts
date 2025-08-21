048d9793afa1b52535c372d847f8722c8911bcde
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-21 14:44:28 +0600

    Error Implementation

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,36 +1,53 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
-import { ZodError } from 'zod';
+import z, { ZodError, ZodIssue } from 'zod';
+import { issue } from 'zod/v4/core/util.cjs';
+import { TErrorSource } from '../interface/error';
+import config from '../config';
 
 // eslint-disable-next-line prettier/prettier
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next,
 ) => {
-    let statusCode = err.statusCode || 500;
+    const statusCode = err.statusCode || 500;
     let message = err.message || 'Something went wrong';
 
-    type TErrorSource = {
-        path: string | number;
-        message: string;
-    }[];
-
-    let errorSources: TErrorSource = [{
-        path: '',
-        message: 'Something went wrong'
-
-    },
+    const errorSources: TErrorSource = [
+        {
+            path: '',
+            message: 'Something went wrong',
+        },
     ];
 
-    if (err instanceof ZodError) {
-        statusCode = 400;
-        message = 'I am Zod Error';
-    }
+    const handleZodError = (err: ZodError) => {
+        const errorSources: TErrorSource = err.issues.map((issue: z.core.$ZodIssue) => {
+            return {
+                path: issue?.path[issue.path.length - 1],
+                message: issue.message,
+            };
+        });
 
+        const statusCode = 400;
+
+        return {
+            statusCode,
+            message: 'Validation Error',
+            errorSources,
+        };
+    };
+
+    if (err instanceof ZodError) {
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources;
+    }
+    //Ultimate retuning from here
     return res.status(statusCode).json({
         success: false,
         message,
         errorSources,
-        // error: err,
+        stack: config.NODE_ENV === 'development' ? err?.stack : null
     });
 };
 
