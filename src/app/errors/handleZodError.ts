7a680371411893fa5e6530b1d923f9010a5973cf
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-21 15:20:54 +0600

    Mongoose first layer Validation error handled

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,22 @@
+import z, { ZodError } from "zod";
+import { TErrorSources } from "../interface/error";
+
+const handleZodError = (err: ZodError) => {
+    const errorSources: TErrorSources = err.issues.map((issue: z.core.$ZodIssue) => {
+        return {
+            path: issue?.path[issue.path.length - 1],
+            message: issue.message,
+        };
+    });
+
+    const statusCode = 400;
+
+    return {
+        statusCode,
+        message: 'Validation Error',
+        errorSources,
+    };
+};
+
+
+export default handleZodError;
--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,66 +1,42 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
-import z, { ZodError, ZodIssue } from 'zod';
-import { issue } from 'zod/v4/core/util.cjs';
-import { TErrorSource } from '../interface/error';
+import { ZodError } from 'zod';
+import { TErrorSources } from '../interface/error';
 import config from '../config';
+import handleZodError from '../errors/handleZodError';
+import handleValidationError from '../errors/handleValidationError'; // <-- Added import
 
-// eslint-disable-next-line prettier/prettier
-const globalErrorHandler: ErrorRequestHandler = (err, req, res, next,
-) => {
-    const statusCode = err.statusCode || 500;
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    let statusCode = err.statusCode || 500;
     let message = err.message || 'Something went wrong';
 
-    const errorSources: TErrorSource = [
+    let errorSources: TErrorSources = [
         {
             path: '',
             message: 'Something went wrong',
         },
     ];
 
-    const handleZodError = (err: ZodError) => {
-        const errorSources: TErrorSource = err.issues.map((issue: z.core.$ZodIssue) => {
-            return {
-                path: issue?.path[issue.path.length - 1],
-                message: issue.message,
-            };
-        });
-
-        const statusCode = 400;
-
-        return {
-            statusCode,
-            message: 'Validation Error',
-            errorSources,
-        };
-    };
-
     if (err instanceof ZodError) {
         const simplifiedError = handleZodError(err);
-        statusCode = simplifiedError?.statusCode;
-        message = simplifiedError?.message;
-        errorSources = simplifiedError?.errorSources;
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    } else if (err?.name === 'ValidationError') {
+        const simplifiedError = handleValidationError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
     }
-    //Ultimate retuning from here
+
+    // Ultimate return from here
     return res.status(statusCode).json({
         success: false,
         message,
         errorSources,
-        stack: config.NODE_ENV === 'development' ? err?.stack : null
+        stack: config.NODE_ENV === 'development' ? err?.stack : null,
+        // err,
     });
 };
 
 export default globalErrorHandler;
-
-// Pattern
-/*
-success
-message 
-errorSources: [
-path: '',
-message:''
-]
-
-stack
-*/
