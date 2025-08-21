0db7c6687a62bfc6c3f6cf67a03a7b64700ee8f9
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-21 14:27:44 +0600

    Error Patterns in Zod And Mongoose

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,21 +1,49 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction, ErrorRequestHandler } from 'express';
+import { ZodError } from 'zod';
 
-const globalErrorHandler = (
-    err: any,
-    req: Request,
-    res: Response,
-    next: NextFunction,
+// eslint-disable-next-line prettier/prettier
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next,
 ) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Something went wrong';
+    let statusCode = err.statusCode || 500;
+    let message = err.message || 'Something went wrong';
+
+    type TErrorSource = {
+        path: string | number;
+        message: string;
+    }[];
+
+    let errorSources: TErrorSource = [{
+        path: '',
+        message: 'Something went wrong'
+
+    },
+    ];
+
+    if (err instanceof ZodError) {
+        statusCode = 400;
+        message = 'I am Zod Error';
+    }
 
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err,
+        errorSources,
+        // error: err,
     });
 };
 
 export default globalErrorHandler;
+
+// Pattern
+/*
+success
+message 
+errorSources: [
+path: '',
+message:''
+]
+
+stack
+*/
