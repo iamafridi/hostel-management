a7e0ca5940fb891a4e15473877c0966967ad84b4
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-22 14:58:21 +0600

    11000 error handled

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,7 @@ import config from '../config';
 import handleZodError from '../errors/handleZodError';
 import handleValidationError from '../errors/handleValidationError'; // <-- Added import
 import handleCastError from '../errors/handleCastError';
+import handleDuplicateError from '../errors/handleDuplicateError';
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     let statusCode = err.statusCode || 500;
@@ -30,6 +31,11 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         errorSources = simplifiedError.errorSources;
     } else if (err?.name === 'CastError') {
         const simplifiedError = handleCastError(err);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSources = simplifiedError.errorSources;
+    } else if (err?.code === 11000) {
+        const simplifiedError = handleDuplicateError(err);
         statusCode = simplifiedError.statusCode;
         message = simplifiedError.message;
         errorSources = simplifiedError.errorSources;
