91e6b9df0b85cbfb2c2ab17d895ec87802a3fafa
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2025-08-28 01:51:07 +0600

    Created Model for course

---
+++ src/app/modules/course/course.model.ts
@@ -0,0 +1,48 @@
+import { model, Schema } from 'mongoose';
+import { TCourse, TPrerequisiteCourses } from './course.interface';
+
+const preRequisiteCoursesSchema = new Schema<TPrerequisiteCourses>({
+    course: {
+        type: Schema.Types.ObjectId
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+const courseSchema = new Schema<TCourse>({
+    title: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+    },
+
+    prefix: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    code: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+
+    credits: {
+        type: Number,
+        trim: true,
+        required: true,
+    },
+    dues: {
+        type: Number,
+    },
+    preRequisiteCourses: [preRequisiteCoursesSchema],
+
+});
+
+
+//Model creation 
+export const course = model<TCourse>('Course', courseSchema)
